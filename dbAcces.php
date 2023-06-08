187579049fdec53909e4cc80014c98f11bbf8c1b
Jorge <jocargi04@gmail.com>
2023-06-08 11:40:44 +0200

    fichero que permite el acceso a al base de datos

---
+++ dbAcces.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "hospital";
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
