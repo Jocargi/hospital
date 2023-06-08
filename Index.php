bf1f421fa3ffa16e8dd23c3a2698d923d5733677
Jorge <jocargi04@gmail.com>
2023-06-08 11:37:09 +0200

    estilo de header

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    require_once("dbAcces.php");
+    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="index.css">
+    
+</head>
+<body>
+    <header>
+    <h1>Gestion de Hospital</h1>
+    <div>
+      
+        <a href="" id="subCabecera">Agregar</a>
+    </div>
+    </header>
+    
+
+
+    
+    
+</body>
+</html>
