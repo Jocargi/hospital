1d6e970452fc8e4bf88298eb07b0669cea3234fa
Jorge <jocargi04@gmail.com>
2023-06-09 12:32:50 +0200

    tabla terminada a falta de editar los botones

--- Index.php
+++ Index.php
@@ -15,14 +15,50 @@
     <header>
     <h1>Gestion de Hospital</h1>
     <div>
-      
         <a href="" id="subCabecera">Agregar</a>
     </div>
     </header>
+    <?php
+    $pdo;
+   $stmt = $pdo->prepare("SELECT * FROM pacientes limit 10");
+   $stmt->execute();
+    $row=1;
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>sip</th>";
+    echo "<th>dni</th>";
+    echo "<th>nombre</th>";
+    echo "<th>apellido1</th>";
+    echo "<th>apellido2</th>";
+    echo "<th>telefono</th>";
+    echo "<th>fecha_nacimiento</th>";
+    echo "<th>Acciones</th>";
+  
+  
+
     
 
-
-    
-    
+    // Recorrer los resultados y mostrar los datos en la tabla
+    while ($row = $stmt->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['sip'] . "</td>";
+        echo "<td>" . $row['dni'] . "</td>";
+        echo "<td>" . $row['nombre'] . "</td>";
+        echo "<td>" . $row['apellido1'] . "</td>";
+        echo "<td>" . $row['apellido2'] . "</td>";
+        echo "<td>" . $row['telefono'] . "</td>";
+        echo "<td>" . $row['localidad'] . "</td>";
+        echo "<td>";
+        echo "<a href='actualizar.php?id=" . $row['id'] . "'>Actualizar</a>";
+        echo "<a href='eliminar.php?id=" . $row['id'] . "'>Eliminar</a>";
+        echo "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    ?>
 </body>
+
 </html>
