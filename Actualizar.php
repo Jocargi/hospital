13e9fc680ff05895f479769dd468c2027b0ba9db
Jorge <jocargi04@gmail.com>
2023-06-09 16:27:22 +0200

    estilo de la pagina de actualizar creado

--- Actualizar.php
+++ Actualizar.php
@@ -4,33 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actualizar</title>
+    <link rel="stylesheet" href="Actualizar.css">
+    
 </head>
 <body>
-<h1>Modificar Usuario</h1>
-    <form action="actualizar.php" method="POST">
+<header>
+    <h1>Modificar Usuario</h1>
+</header>
+
+<section>
+<form action="actualizar.php" method="POST">
         <label for="id">ID:</label>
         <input type="text" name="id" >
         
         <label for="Sip">SIP:</label>
-        <input type="text" name="sip">
+        <input type="number" name="sip">
         
         <label for="Dni">DNI:</label>
-        <input type="email" name="dni">
+        <input type="text" name="dni">
 
         <label for="Dni">nombre:</label>
         <input type="text" name="nombre">
 
-        <label for="Dni">apellido1:</label>
-        <input type="email" name="apellido1">
+        <label for="apellido1">apellido1:</label>
+        <input type="text" name="apellido1">
 
-        <label for="Dni">apellido2:</label>
-        <input type="email" name="apelldo2">
+        <label for="apellido2">apellido2:</label>
+        <input type="text" name="apelldo2">
         
         <label for="telefono">Teléfono:</label>
-        <input type="text" name="telefono">
+        <input type="tel" name="telefono">
         
         <input type="submit" value="Actualizar">
     </form>
+
+</section>
+
+   
 </body>
 </html>
 
