7a828e8f85b409c951fabf024a1e92fb73a7be37
Jorge <jocargi04@gmail.com>
2023-06-10 17:15:43 +0200

    campo sexo añadido

--- Actualizar.php
+++ Actualizar.php
@@ -2,7 +2,6 @@
 require_once("dbAcces.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtienes los datosde los pacientes a través de la base del metodo post
     $id = $_POST["id"];
     $sip = $_POST["sip"];
     $dni = $_POST["dni"];
@@ -11,10 +10,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $apellido2 = $_POST["apellido2"];
     $telefono = $_POST["telefono"];
     $localidad = $_POST["localidad"];
+    $sexo = $_POST["sexo"];
 
-    // Aqui lo que sucede es que se realiza la actualización y el bimParam por ejempl 
-    //$stmt->bindParam(":dni", $dni);: Vincula el parámetro :dni de la consulta preparada con la variable $dni que contiene el valor actualizado del DNI  
-    $stmt = $pdo->prepare("UPDATE pacientes SET sip = :sip, dni = :dni, nombre = :nombre, apellido1 = :apellido1, apellido2 = :apellido2, telefono = :telefono, localidad = :localidad WHERE id = :id");
+    $stmt = $pdo->prepare("UPDATE pacientes SET sip = :sip, dni = :dni, nombre = :nombre, apellido1 = :apellido1, apellido2 = :apellido2, telefono = :telefono, localidad = :localidad, sexo = :sexo WHERE id = :id");
+    $stmt->bindParam(":id", $id);
     $stmt->bindParam(":sip", $sip);
     $stmt->bindParam(":dni", $dni);
     $stmt->bindParam(":nombre", $nombre);
@@ -22,23 +21,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bindParam(":apellido2", $apellido2);
     $stmt->bindParam(":telefono", $telefono);
     $stmt->bindParam(":localidad", $localidad);
-    $stmt->bindParam(":id", $id);
+    $stmt->bindParam(":sexo", $sexo);
     $stmt->execute();
 
-    // Te redirecciona a la pagina principar si no hay nadad mal
     header("Location: index.php");
     exit();
 } else {
-    // Obtener el ID del paciente de la URL
     $id = $_GET["id"];
 
-    // Obtener los datos del paciente según su ID
     $stmt = $pdo->prepare("SELECT * FROM pacientes WHERE id = :id");
     $stmt->bindParam(":id", $id);
     $stmt->execute();
     $paciente = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Verificar si se encontró el paciente
     if (!$paciente) {
         echo "Paciente no encontrado.";
         exit();
@@ -50,30 +45,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Actualizar</title>
     <link rel="stylesheet" href="Actualizar.css">
 </head>
 <body>
-    <header> <h1>Actulizar clientes</h1></header>
-<form action="actualizar.php" method="POST">
-    <label for="id">ID:</label>
-    <input type="text" name="id" value="<?php echo $paciente['id']; ?>">
-    <label for="sip">SIP:</label>
-    <input type="text" name="sip" value="<?php echo $paciente['sip']; ?>">
-    <label for="dni">DNI:</label>
-    <input type="text" name="dni" value="<?php echo $paciente['dni']; ?>">
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" value="<?php echo $paciente['nombre']; ?>">
-    <label for="apellido1">Apellido1:</label>
-    <input type="text" name="apellido1" value="<?php echo $paciente['apellido1']; ?>">
-    <label for="apellido2">Apellido2:</label>
-    <input type="text" name="apellido2" value="<?php echo $paciente['apellido2']; ?>">
-    <label for="telefono">Teléfono:</label>
-    <input type="tel" name="telefono" value="<?php echo $paciente['telefono']; ?>">
-    <label for="localidad">Localidad:</label>
-    <input type="text" name="localidad" value="<?php echo $paciente['localidad']; ?>">
-    <input type="submit" value="Actualizar">
-</form>
-    
+    <header>
+        <h1>Modificar Usuario</h1>
+    </header>
+    <section>
+        <form action="actualizar.php" method="POST">
+            <label for="id">ID:</label>
+            <input type="text" name="id" value="<?php echo $paciente['id']; ?>">
+            <label for="sip">SIP:</label>
+            <input type="text" name="sip" value="<?php echo $paciente['sip']; ?>">
+            <label for="dni">DNI:</label>
+            <input type="text" name="dni" value="<?php echo $paciente['dni']; ?>">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" value="<?php echo $paciente['nombre']; ?>">
+            <label for="apellido1">Apellido1:</label>
+            <input type="text" name="apellido1" value="<?php echo $paciente['apellido1']; ?>">
+            <label for="apellido2">Apellido2:</label>
+            <input type="text" name="apellido2" value="<?php echo $paciente['apellido2']; ?>">
+            <label for="telefono">Teléfono:</label>
+            <input type="tel" name="telefono" value="<?php echo $paciente['telefono']; ?>">
+            <label for="localidad">Localidad:</label>
+            <input type="text" name="localidad" value="<?php echo $paciente['localidad']; ?>">
+            <label for="sexo">Sexo:</label>
+            <select name="sexo">
+                <option value="Hombre" <?php if($paciente['sexo'] == 'Hombre') echo 'selected'; ?>>Hombre</option>
+                <option value="Mujer" <?php if($paciente['sexo'] == 'Mujer') echo 'selected'; ?>>Mujer</option>
+            </select>
+            <input type="submit" value="Actualizar">
+        </form>
+    </section>
 </body>
 </html>
+
--- Index.php
+++ Index.php
@@ -2,69 +2,64 @@
 <html lang="en">
 <?php
     require_once("dbAcces.php");
-    ?>
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
     <link rel="stylesheet" href="index.css">
-    
 </head>
 <body>
     <header>
-    <h1>Gestion de Hospital</h1>
-    <div>
-        <a href="" id="subCabecera">Agregar</a>
-    </div>
+        <h1>Gestión de Hospital</h1>
+        <div>
+            <a href="" id="subCabecera">Agregar</a>
+        </div>
     </header>
+
     <?php
-    $pdo;
-   $stmt = $pdo->prepare("SELECT * FROM pacientes limit 10");
-   $stmt->execute();
-    $row=1;
-
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>ID</th>";
-    echo "<th>sip</th>";
-    echo "<th>dni</th>";
-    echo "<th>nombre</th>";
-    echo "<th>apellido1</th>";
-    echo "<th>apellido2</th>";
-    echo "<th>telefono</th>";
-    echo "<th>localidad</th>";
-    echo "<th>Acciones</th>";
-  
-  
-
-    
-
-    // Recorrer los resultados y mostrar los datos en la tabla
-    while ($row = $stmt->fetch()) {
-        echo "<tr>";
-        echo "<td>" . $row['id'] . "</td>";
-        echo "<td>" . $row['sip'] . "</td>";
-        echo "<td>" . $row['dni'] . "</td>";
-        echo "<td>" . $row['nombre'] . "</td>";
-        echo "<td>" . $row['apellido1'] . "</td>";
-        echo "<td>" . $row['apellido2'] . "</td>";
-        echo "<td>" . $row['telefono'] . "</td>";
-        echo "<td>" . $row['localidad'] . "</td>";
-        echo "<td>";
-        echo "<a href='Actualizar.php?id=" . $row['id'] . "'>Actualizar |</a>";
-        echo "<a href='eliminar.php?id=" . $row['id'] . "'>Eliminar</a>";
-        echo "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-
-
-
-
-
-
+        $pdo;
+        $stmt = $pdo->prepare("SELECT * FROM pacientes LIMIT 10");
+        $stmt->execute();
     ?>
-</body>
 
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>SIP</th>
+                <th>DNI</th>
+                <th>Nombre</th>
+                <th>Apellido1</th>
+                <th>Apellido2</th>
+                <th>Teléfono</th>
+                <th>Localidad</th>
+                <th>Sexo</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                while ($row = $stmt->fetch()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['sip'] . "</td>";
+                    echo "<td>" . $row['dni'] . "</td>";
+                    echo "<td>" . $row['nombre'] . "</td>";
+                    echo "<td>" . $row['apellido1'] . "</td>";
+                    echo "<td>" . $row['apellido2'] . "</td>";
+                    echo "<td>" . $row['telefono'] . "</td>";
+                    echo "<td>" . $row['localidad'] . "</td>";
+                    echo "<td>" . $row['sexo'] . "</td>";
+                    echo "<td>";
+                    echo "<a href='Actualizar.php?id=" . $row['id'] . "'>Actualizar</a> | ";
+                    echo "<a href='eliminar.php?id=" . $row['id'] . "'>Eliminar</a>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
 </html>
